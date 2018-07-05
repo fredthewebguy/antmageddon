f9172ceb4a417ac0860af2dbefe825eacd416bf7
Fred the Web Guy <35805971+fredthewebguy@users.noreply.github.com>
2018-07-05 06:45:20 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+	
+	<head>
+		
+		<meta charset="utf-8">
+		
+		<title>WAYNE'S ANTMAGEDDON!!!</title>	
+		
+		<meta http-equiv="Pragma" content="no-cache">
+		<meta name="viewport" content="width=device-width,initial-scale=1">
+		
+		<link rel="stylesheet" href="style.css">
+	
+		<!--[if lt IE 9]>
+		<script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+		<![endif]-->
+	
+	</head>
+	
+	<body>
+	
+		<header>
+			<h1><span>Wayne's</span>Antmageddon!!!</h1>
+		</header>
+		
+		<p class="alert">Sorry, this game is only playable on a desktop computer!</p>
+	
+		<script src="js/game.js"></script>
+		
+		<h2>How to Play</h2>
+		<p>Using your arrow keys to lead Wayne to an ant so he can stomp on it.</p>
+	
+		<p class="blink_me"><a href="http://www.nickperon.com/waynes-antmageddon/">Start over</a></p>
+	
+		<p class="copyright">&copy; by <a href="#">Freddy</a></p>
+		
+		<div class="audio-wrapper">
+			<audio loop="loop" autoplay="autoplay" controls="controls">  
+				<source src="bach.ogg" />  
+				<source src="bach.mp3" />  
+			</audio> 
+		</div>
+		
+	</body>
+</html>
